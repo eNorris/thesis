0f0be37a63c89d88e341230676641bab7c8316f9
Edward Norris <etnc6d@mst.edu>
2016-03-24 21:57:14 -0500

    Updated the QuadGlWidget to handle mouse events

--- doctors/quadglwidget.cpp
+++ doctors/quadglwidget.cpp
@@ -1,6 +1,13 @@
 #include "quadglwidget.h"
 
-QuadGlWidget::QuadGlWidget(QWidget *parent) : QOpenGLWidget(parent)
+#include <QMouseEvent>
+#include <math.h>
+#include <QDebug>
+
+QuadGlWidget::QuadGlWidget(QWidget *parent) : QOpenGLWidget(parent),
+    m_xRot(0),
+    m_yRot(0),
+    m_zRot(0)
 {
     QSurfaceFormat format;
     format.setDepthBufferSize(24);
@@ -9,7 +16,7 @@ QuadGlWidget::QuadGlWidget(QWidget *parent) : QOpenGLWidget(parent)
 
 QuadGlWidget::~QuadGlWidget()
 {
-
+    cleanup();
 }
 
 void QuadGlWidget::initializeGL()
@@ -27,4 +34,69 @@ void QuadGlWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // paint the picture
+}
+
+void QuadGlWidget::setXRotation(int angle)
+{
+    // Use float later so this isn't necessary
+    while (angle < 0)
+        angle += 360 * 16;
+    while (angle > 360 * 16)
+        angle -= 360 * 16;
+
+    m_xRot = angle;
+    update();
+}
+
+void QuadGlWidget::setYRotation(int angle)
+{
+    while (angle < 0)
+        angle += 360 * 16;
+    while (angle > 360 * 16)
+        angle -= 360 * 16;
+
+    m_yRot = angle;
+    update();
+}
+
+void QuadGlWidget::setZRotation(int angle)
+{
+    while (angle < 0)
+        angle += 360 * 16;
+    while (angle > 360 * 16)
+        angle -= 360 * 16;
+
+    m_zRot = angle;
+    update();
+}
+
+void QuadGlWidget::mousePressEvent(QMouseEvent *event)
+{
+    qDebug() << "QuadGlWidget::Press event";
+    m_lastPos = event->pos();
+}
+
+void QuadGlWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    qDebug() << "QuadGlWidget::Move event";
+    int dx = event->x() - m_lastPos.x();
+    int dy = event->y() - m_lastPos.y();
+
+    if(event->buttons() & Qt::LeftButton)
+    {
+        setXRotation(m_xRot + 8 * dy);
+        setYRotation(m_yRot + 8 * dx);
+    }
+    else if(event->buttons() & Qt::RightButton)
+    {
+        setXRotation(m_xRot + 8 * dy);
+        setZRotation(m_zRot + 8 * dx);
+    }
+    m_lastPos = event->pos();
+}
+
+void QuadGlWidget::cleanup()
+{
+    makeCurrent();
+    doneCurrent();
 }
--- doctors/quadglwidget.h
+++ doctors/quadglwidget.h
@@ -16,9 +16,21 @@ protected:
     void initializeGL() Q_DECL_OVERRIDE;
     void resizeGL(int w, int h) Q_DECL_OVERRIDE;
     void paintGL() Q_DECL_OVERRIDE;
+    void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+    void mouseMoveEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+
+public slots:
+    void setXRotation(int angle);
+    void setYRotation(int angle);
+    void setZRotation(int angle);
+    void cleanup();
 
 private:
     QMatrix4x4 m_projection;
+    int m_xRot;
+    int m_yRot;
+    int m_zRot;
+    QPoint m_lastPos;
 };
 
 #endif // QUADGLWIDGET_H
