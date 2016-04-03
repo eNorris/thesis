c00b2c3f66383ab744ca485cf9e93c8c565e2f66
Edward Norris <etnc6d@mst.edu>
2016-04-02 22:54:13 -0500

    Added function to generate an array to iterate through properly for each octant. This functionality isn't currently used.

--- doctors/mesh.h
+++ doctors/mesh.h
@@ -35,6 +35,16 @@ public:
     std::vector<float> Ayz;  // Replaces DB
     std::vector<float> Axz;  // Replaces DC
 
+                                             // mu xi eta
+    std::vector<unsigned int> orderOctant1;  // + + +
+    std::vector<unsigned int> orderOctant2;  // - + +
+    std::vector<unsigned int> orderOctant3;  // - - +
+    std::vector<unsigned int> orderOctant4;  // + - +
+    std::vector<unsigned int> orderOctant5;  // + + -
+    std::vector<unsigned int> orderOctant6;  // - + -
+    std::vector<unsigned int> orderOctant7;  // - - -
+    std::vector<unsigned int> orderOctant8;  // + - -
+
     std::vector<float> vol;
 
     std::vector<unsigned short> zoneId;
@@ -44,6 +54,8 @@ public:
     int voxelCount() const;
     int xjmp() const;
     int yjmp() const;
+
+    std::vector<unsigned int> &getOctantOrder(const float mu, const float xi, const float eta);
 
 public slots:
     void remesh(int xelems, int yelems, int zelems, const Config *config, const Quadrature *quad);
