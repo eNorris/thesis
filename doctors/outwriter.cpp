d20078de54176cb6ac13c791661280fd070df3a6
Edward Norris <etnc6d@mst.edu>
2016-04-02 22:52:24 -0500

    Added a new class with static methods to write the flux to a file

---
+++ doctors/outwriter.cpp
@@ -0,0 +1,7 @@
+#include "outwriter.h"
+
+OutWriter::OutWriter()
+{
+
+}
+
---
+++ doctors/outwriter.h
@@ -0,0 +1,15 @@
+#ifndef OUTWRITER_H
+#define OUTWRITER_H
+
+
+class OutWriter
+{
+public:
+    OutWriter();
+
+signals:
+
+public slots:
+};
+
+#endif // OUTWRITER_H
