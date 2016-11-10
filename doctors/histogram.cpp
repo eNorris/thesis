9d68dda4eeab50eab0a735d11427a8a9174a09fb
Edward Norris <etnc6d@mst.edu>
2016-11-09 18:22:51 -0600

    Finished the Histogram class

--- doctors/histogram.cpp
+++ doctors/histogram.cpp
@@ -1,7 +1,24 @@
 #include "histogram.h"
 
-Histogram::Histogram()
+Histogram::Histogram(float min, float max, float bins) : m_min(min), m_max(max), m_bins(bins)
 {
+    if(bins >= 0)
+    {
+        m_upperBounds.resize(m_bins);
+        m_counts.resize(m_bins, 0);
+    }
+
+    const float dx = (m_max-m_min)/m_bins;
+    float v = dx;
+    int indx = 0;
+
+    while(v < m_max)
+    {
+        m_upperBounds[indx] = v;
+        v += dx;
+        indx++;
+    }
+    m_upperBounds[m_upperBounds.size()-1] = m_max;
 
 }
 
@@ -10,3 +27,34 @@ Histogram::~Histogram()
 
 }
 
+void Histogram::insert(float x)
+{
+    if(x < m_min)
+        m_underflows++;
+    else if(x > m_max)
+        m_overflows++;
+    else
+    {
+        for(int indx = 0; indx < m_upperBounds.size(); indx++)
+        {
+            if(m_upperBounds[indx] > x)
+            {
+                m_counts[indx]++;
+                break;
+            }
+        }
+    }
+    return;
+}
+
+std::ostream& operator<<(std::ostream& out, const Histogram& hist)
+{
+    out << hist.m_min << '\t' << hist.m_underflows << '\n';
+    for(int i = 0; i < hist.m_bins; i++)
+    {
+        out << hist.m_upperBounds[i] << '\t' << hist.m_counts[i] << '\n';
+    }
+    out << "0\t" << hist.m_overflows;
+
+    return out;
+}
--- doctors/histogram.h
+++ doctors/histogram.h
@@ -1,12 +1,28 @@
 #ifndef HISTOGRAM_H
 #define HISTOGRAM_H
 
+#include <vector>
+#include <iostream>
 
 class Histogram
 {
 public:
-    Histogram();
+    Histogram(float min, float max, float bins);
     ~Histogram();
+
+    void insert(float x);
+
+    friend std::ostream& operator<<(std::ostream& out, const Histogram& hist);
+
+protected:
+    int m_underflows;
+    int m_overflows;
+    std::vector<int> m_counts;
+    std::vector<float> m_upperBounds;
+
+    float m_min;
+    float m_max;
+    float m_bins;
 };
 
 #endif // HISTOGRAM_H
