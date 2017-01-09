848a3e2f19d90cea9f5bd1362beacec4a40997e9
eNorris <etnc6d@mst.edu>
2017-01-09 00:00:40 -0600

    Added the spherical harmonic functionality

--- doctors/legendre.h
+++ doctors/legendre.h
@@ -16,6 +16,19 @@ public:
     float operator()(const int l, const int m, const float x);
 };
 
+class SphericalHarmonic
+{
+public:
+    SphericalHarmonic();
+    ~SphericalHarmonic();
+
+    float normConst(const int l, const int m);
+    float operator()(const int l, const int m, const float theta, const float phi);
+
+protected:
+    AssocLegendre m_assoc;
+};
+
 
 
 #endif // LEGENDRE_H
