17fc5b1a90bbcac428b6b70943dee0bdecaa48e2
Edward Norris <etnc6d@mst.edu>
2017-06-02 20:07:17 -0500

    Added a file that splits the meshtal file into smaller meshes

---
+++ python/ct_data_reader/main/meshsplitter.py
@@ -0,0 +1,30 @@
+__author__ = 'etnc6d'
+
+filename = '/media/Storage/thesis/mcnp.gitignore/meshtar'
+
+file = open(filename, 'r')
+#with open(filename, 'r') as file:
+
+indx = 1
+#fin = open(filename, 'r')
+fout = open(filename+str(indx), 'w')
+
+lines = file.readlines()
+
+for line in lines:
+    l = line.lstrip()
+    if l.startswith("Energy"):
+        fout.close()
+        indx += 1
+        fout = open(filename+str(indx), 'w')
+
+    if not l.startswith('Total'):
+        try:
+            float(l.split()[0])
+            fout.write(line)
+        except:
+            pass
+fout.close()
+file.close()
+
+print("Finished.")
